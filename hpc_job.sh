23310561a42aa66cfbea3d28d474c499f9e7731b
Faiz Palwala <faiz.palwala@gmail.com>
2026-07-18 12:41:06 +0100

    fixed image paths

--- hpc_job.sh
+++ hpc_job.sh
@@ -61,9 +61,9 @@ cd "$TMPDIR/repo/src"
 #     > "$REPO_DIR/logs/datagen_execution_${SLURM_JOB_ID}.log" 2>&1
 
 python instantid_adapter.py \
-  --id_image ../data/processed/SFHQ_pt1_00000006_face.jpg \
-  --style_image ../data/processed/SFHQ_pt1_00000226_face.jpg \
-  --output_path ../data/tmp \
+  --id_image ../../data/processed/SFHQ_pt1_00000006_face.jpg \
+  --style_image ../../data/processed/SFHQ_pt1_00000226_face.jpg \
+  --output_path ../../data/tmp \
   --seed 42 \
   --width 1024 \
   --height 1024 \
